c2b1cee41962ed5ede21198d4f47e4919c891ca2
Don Brown <143838430+seagullreport@users.noreply.github.com>
2024-10-31 10:47:32 -0700

    More verbiage updates.

--- src/content.js
+++ src/content.js
@@ -35,19 +35,19 @@ export const projectsSkills = [
         name: "Javascript",
         experience: "2006 - Present",        
         synopsis:
-          "Development libraries make Javascript a very handy tool for web APIs and asynchronous calls. Now Javascript drives the V8 Engine expanded developemnt environmnets such as Node.js. This portfolio website is created using Javascript architectures Node.js with React and Framer-Motion.",
+          "Development libraries make Javascript a very handy tool for web APIs and asynchronous calls. Now Javascript drives the V8 Engine expanded development environments such as Node.js. This portfolio website is created using Javascript architectures Node.js with React and Framer-Motion.",
       },
       {
         name: "TypeScript (TSX)",
         experience: "2020 - Present",        
         synopsis:
-          "Implementing Typescript (TSX) saved me from writing custom Javascript types myself! Writing and compiling with TSX prevents innumerable runtime errors from reaching the CID cycle. Prior to TSX the devlopers (I, we) were compelled to write custom scripts to type and validate objects, or risk unknown and often silect exceptions upon deployment.",
+          "Implementing Typescript (TSX) saved me from writing custom Javascript types myself! Writing and compiling with TSX prevents innumerable runtime errors from reaching the CID cycle. Prior to TSX the developers (I, we) were compelled to write custom scripts to type and validate objects, or risk unknown and often silent exceptions upon deployment.",
       },
       {
         name: "C#",
         experience: "2006 - Present",        
         synopsis:
-          "I have constructed a wide variety solutions utilizing the functionality of C# and ASP.NET. C# is an ubiquitous script used for countless implementations. In learning C# I have also learned to comprehend similar scripts: Java, C+, Python and others.",
+          "I have constructed a wide variety solutions utilizing the functionality of C# and ASP.NET. C# is an ubiquitous script used for countless implementations. As development lead, I focused on creating ASP.NET web services with SQL Server repositories to securely collect and analyze research data for investigators. In learning C#, I have transposed to similar scripts such as Javascript, C+, Python and more.",
       },
       {
         name: "Python",
@@ -59,13 +59,13 @@ export const projectsSkills = [
         name: "PowerShell",
         experience: "2010 - Present",        
         synopsis:
-          "PowerShell enables piping of objects from server to server, porting files, porting logic, examining remote server status and countless other tasks end-to-end. PowerShell allows engineers to develop robust automated processes accross many server nodes.",
+          "PowerShell enables piping of objects from server to server, porting files, porting logic, examining remote server status and countless other tasks end-to-end. PowerShell allows engineers to develop robust automated processes across many server nodes. With PowerShell and ASP.NET, I developed internal 'Malfunctioning Service Reporting' system, which gathered essential runtime error data across servers for use in code improvements.",
       },      
       {
         name: "Bash",
         experience: "2019 - Present",
         synopsis:
-          "Cousin to PowerShell, Bash is Unix Shell that easily manipulates files and automates manual processes. Syntactically friendlier than PowerShell, it offers a common set of decision operators and function declarations which are intuitive to most experienced developers. GitBash for windows has become an indispensible tool in using Github resources to the fullest. Needless to say this site is developed in part using GitBash.",
+          "Cousin to PowerShell, Bash is Unix Shell that easily manipulates files and automates manual processes. Syntactically friendlier than PowerShell, it offers a common set of decision operators and function declarations which are intuitive to most experienced developers. GitBash for windows has become an indispensible tool in using Github resources to the fullest. This folio is managed and deployed using GitBash.",
       },
     ],
   },
@@ -82,13 +82,13 @@ export const projectsSkills = [
         name: "tSQLt Unit Tests",
         experience: "2022 - Present",        
         synopsis:
-          "Indispensible unit-testing tool when developing and extending SQL Server solutions. Well-formed tSQLt enforces business rules at the front line. Atomic-level logic checks, compilation checks and custom schematics checks extremely powerful when adding schematics to CID. Uniformity of code is another welcome by-product. Robust SQL Server data schematics begin with tSQLt Unit Tests.",
+          "Indispensible unit-testing tool when developing and extending SQL Server solutions. Well-formed tSQLt enforces business rules at the front line. Atomic-level logic checks, compilation checks and custom schematics checks extremely powerful when adding schematics to CID. Uniformity of code is another welcome by-product. Applying tSQLt unit tests virtually eliminated schematic run-time errors when applying late-breaking SQL changes.",
       },
       {
         name: "Stored Procedure",
         experience: "2006 - Present",        
         synopsis:
-          "Stored Procedures: securable SQL objects, which present data, manipulate data using parameters, provide business logic, and communicate data within Server layers. Custom tSQLt Unit Tests combined ensures each storeds procedure meets minimum standards and any custom logical standards necessary. Extensively used to enforce business logic and maintain data security. Stored procedures tackle the heavy processing and allow the front-end systems to render information as fast as possible.",
+          "Stored Procedures: securable SQL objects, which present data, manipulate data using parameters, provide business logic, and communicate data within Server layers. Custom tSQLt Unit Tests combined ensures each stored procedure meets minimum standards and any custom logical standards necessary. Extensively used to enforce business logic and maintain data security. Stored procedures tackle the heavy processing and allow the front-end systems to render information as fast as possible.",
       },
       {
         name: "Optimization",
@@ -100,13 +100,13 @@ export const projectsSkills = [
         name: "Data Security",
         experience: "2006 - Present",        
         synopsis:
-          "Database accounts, database roles, encryption, user accounts, user roles and user schemas all are employed to define security unique to the business solution. Majority of my projects have chosen SQL Server for its reliabilty, utility and ubiquity when dealing with sensitive data. But even the simpler things gretly improve security: user training. Most intrusions are the result of human error rather than technical weakness. Well-trained users are the strongest front-line defense.",
+          "Database accounts, database roles, encryption, user accounts, user roles and user schemas all are employed to define security unique to the business solution. Majority of my projects have chosen SQL Server for its reliability, utility and ubiquity when dealing with sensitive data. But even the simpler things greatly improve security: user training. Most intrusions are the result of human error rather than technical weakness. Well-trained users are the strongest front-line defense.",
       },
       {
         name: "Regular Expression",
         experience: "2008 - Present",
         synopsis:
-          "Regular Expression, or Regex, is an often overlooked and powerful developer tool for digesting large amounts of data. Regex excel at the string comparisons and manipulations which tend to slow down higher-level scripts.",
+          "Regular Expression, or Regex, is an often overlooked and powerful developer tool for digesting large amounts of data. ETL process can benefit from skillfully applied Regex. String manipulations cause me to think of Regex first to utilize its precision and fast processing power.",
       },
     ],
   },
@@ -117,14 +117,14 @@ export const projectsSkills = [
         name: "NodeJS, React",
         experience: "Experience: 2022 - Present",
         synopsis:
-          "This site runs with NodeJS runtime using React JS architechture. Elements are React components. Complex animations implement Framer-Motion. I have developed solutions using Razor, Bootstrap, php and Web Forms and I find React to be the most exciting and least error-proned. Front-end development is extremely fast and rosbust with this framework. All using Javascript.",
+          "This site runs with NodeJS runtime using React JS architecture. Elements are React components. Complex animations implement Framer-Motion. I have developed solutions using Razor, Bootstrap, php and Web Forms and I find React to be the most exciting and least error-prone. Front-end development is extremely fast and robust with this framework. All using Javascript.",
       },
       {
         name: "TailwindCSS",
         experience: "Experience: 2022 - Present",
         
         synopsis:
-          "This site styled with TailwindCSS which limits or obviates the messiness and overlap of hand-written CSS classes. TailwindCSS is packed with explicit classes,  innate functionality and is easily configured to custom settings. Levereging TailwindCSS, custom website styles are easily designed and specified before any code is written or content delivered. Custom style templates are created with equal ease.",
+          "This site styled with TailwindCSS which limits or obviates the messiness and overlap of hand-written CSS classes. TailwindCSS is packed with explicit classes,  innate functionality and is easily configured to custom settings. Leveraging TailwindCSS, custom website styles are easily designed and specified before any code is written or content delivered. Custom style templates are created with equal ease.",
       },
       {
         name: "Docker",
@@ -153,7 +153,7 @@ export const projectsSkills = [
         name: "Workday ERP Conversion",
         experience: "Workday ERP Conversion",
         synopsis:
-          "Finance softwares had until Fall 2023 to become security-compliant and Workday ERP capable. I oversaw the developent of this technology to replace legacy software for an internal department. We created secure, web-based technology which utilized existing Employer resources to successfully link staff with all necessary IT systems, including Employer Workday. New functionality added to the solution automated tme-consuming tasks that were previously done by hand and eye. New system is documented and easily extensible to meet changing department needs.",
+          "We designed, tested and implemented the extensible, secure web solution to interface Workday and replace business functionality of legacy software. New solution significantly freed staff from manual processes to satisfying the business concerns.",
       },
       {
         name: "Create Web Infrastructure",
@@ -162,10 +162,10 @@ export const projectsSkills = [
           "In 2018, one finance department relied on legacy, outdated FoxPro applications for internal operations. Establishing secure web capabilities became imperative. I provisioned web IIS and SQL servers from central EmployerIT and installed Shibboleth security which leveraged existing Employer domain services, groups and user accounts. Automated migration kept financial data current with SQL Server while development of robust web functionality progressed. Completed solution fully integrated into Workday.",
       },
       {
-        name: "Worktag Translations",
-        experience: "Worktag Translations",        
+        name: "Work Tag Translations",
+        experience: "Work Tag Translations",        
         synopsis:
-          "Workday changes rolled out quickly and backlogs were problematic. I developed scripting technology to automate translation of legacy work codes into Workday-compliant 'Worktags' which were then inserted to the appropriate SQL Server workflow. This eliminated manual processes in place - freeing upstream staff for more critical duties.",
+          "Workday changes rolled out quickly and backlogs were problematic. I developed scripting technology to automate translation of legacy work codes into Workday-compliant 'Work Tags' which were then inserted to the appropriate SQL Server workflow. This eliminated manual processes in place - freeing upstream staff for more critical duties.",
       },
       {
         name: "SQL Script Deployment",
@@ -177,13 +177,13 @@ export const projectsSkills = [
         name: "Pathology Projects",
         experience: "Pathology Projects",
         synopsis:
-          "Twin research projects gathered data on pathologists reviewing digital tissue slides and physical biopsy slides.  We developed custom 'slide zooming' web browsing that tracked participants' interactive behaviors as they reviewed, paused, focused and zoomed. This provided copious amounts of study data despite the small population size of the study. Custom DotNET web application handled enrolling, randomizing and collecting responses from all pariticipants. Project success led to future grant opportunities for the investigators.",
+          "Twin research projects gathered data on pathologists reviewing digital tissue slides and physical biopsy slides.  We developed custom 'slide zooming' web browsing that tracked participants' interactive behaviors as they reviewed, paused, focused and zoomed. This provided copious amounts of study data despite the small population size of the study. Custom DotNET web application handled enrolling, randomizing and collecting responses from all participants. Project success led to future grant opportunities for the investigators.",
       },
       {
         name: "Shibboleth Security",
         experience: "Shibboleth Security",
         synopsis:
-          "Sunsetting legacy department software to replace it with its web-based, Workday-capable solution was my primary task as developemnt lead. I provisioned server resources and implemented Shibboleth security using Employer's existing IT structure. Website security calibrated 'under the hood' using security groups allowed staff to use existing credentials for all logins.",
+          "Retiring legacy department software to replace it with its web-based, Workday-capable solution was my primary task as development lead. I provisioned server resources and implemented Shibboleth security using Employer's existing IT structure. Website security calibrated 'under the hood' using security groups allowed staff to use existing credentials for all logins.",
       },
     ],
   },
@@ -194,7 +194,7 @@ export const projectsSkills = [
         name: "About",
         experience: "About",        
         synopsis:
-          "Don Brown: experienced Software Engineer developing solutions for retail, health care, business-to-business, non-profit and educational environments. Don Brown offers deep IT skills to best fit the business needs with the most scalable and efficient technology. Resumé upon request, please use my Contact Form!",
+          "Don Brown: experienced Software Engineer developing solutions for retail, health care, business-to-business, non-profit and educational environments. Don Brown offers deep IT skills to best fit the business needs with the most scalable and efficient technology. Resume upon request, please use my Contact Form!",
       },
       {
         name: "Hobbies",
